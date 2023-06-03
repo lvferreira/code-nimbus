2119e4581fca27337daec4ec3b47c92dc040f070
Leandro Viola <leandro.vf07@gmail.com>
2023-06-03 17:05:31 -0300

    code-nimbus

--- src/Index.java
+++ src/Index.java
@@ -24,7 +24,7 @@ public class Index {
 		// WebDriver Interface + Browser Driver Class Methods
 		String webdriver = "webdriver.edge.driver";
 		String path = "C:\\Users\\Leandro\\Eclipse-Workspace\\SeleniumWebdriver\\msedgedriver.exe";
-		System.setProperty(webdriver, path);
+			System.setProperty(webdriver, path);
 		
 		// Launch Browser (webdriver.browser.driver -> path)
 		WebDriver driver = new EdgeDriver();
@@ -44,30 +44,31 @@ public class Index {
 			String url = "https://rahulshettyacademy.com/";
 			driver.get(url);
 			driver.get(url+"locatorspractice/");
-			System.out.println(driver.getTitle());
-			System.out.println(driver.getCurrentUrl());
-		}
-			
+				System.out.println(driver.getTitle());
+				System.out.println(driver.getCurrentUrl());
+		}	
 	
 	// Valid password
 		public static void validPassword(WebDriver driver) {
-		driver.findElement(By.id("inputUsername")).sendKeys("lahunkbal");; //#inputUsername
-		driver.findElement(By.name("inputPassword")).sendKeys("rahulshettyacademy");; //inputPassword
-		driver.findElement(By.className("signInBtn")).click(); //.signInBtn
-		//		Thread.sleep(1000);
-			
-		// Declare an explicit wait with a timeout of 10 seconds
-	    WebDriverWait wait = new WebDriverWait(driver, Duration.ofSeconds(10));
-	        
-		// Wait for the element to be visible before interacting with it
-	    WebElement element = wait.until(ExpectedConditions.visibilityOfElementLocated(By.xpath("//p[text()=\"You are successfully logged in.\"]")));
-	        
-	    String loginMessage = element.getText();
-	    Assert.assertEquals(loginMessage, "You are successfully logged in.");
-	    //		String loginMessage = driver.findElement(By.tagName("p")).getText();
-		//		Assert.assertEquals(loginMessage, "You are successfully logged in.");
-				System.out.println(loginMessage);
-	}
+			driver.findElement(By.id("inputUsername")).sendKeys("lahunakbal");; //#inputUsername
+			driver.findElement(By.name("inputPassword")).sendKeys("rahulshettyacademy");; //inputPassword
+			driver.findElement(By.className("signInBtn")).click(); //.signInBtn
+			//		Thread.sleep(1000);
+				
+			// Declare an explicit wait with a timeout of 10 seconds
+		    WebDriverWait wait = new WebDriverWait(driver, Duration.ofSeconds(10));
+		        
+			// Wait for the element to be visible before interacting with it
+		    WebElement element = wait.until(ExpectedConditions.visibilityOfElementLocated(By.xpath("//p[text()=\"You are successfully logged in.\"]")));
+		    String loginMessage = element.getText();
+		    Assert.assertEquals(loginMessage, "You are successfully logged in.");
+		    	//		String loginMessage = driver.findElement(By.tagName("p")).getText();
+				//		Assert.assertEquals(loginMessage, "You are successfully logged in.");
+		    	System.out.println(loginMessage);
+		    String username = "lahunakbal";
+		    Assert.assertEquals(driver.findElement(By.cssSelector("div[class='login-container'] h2")).getText(), "Hello "+username+",");
+			driver.findElement(By.xpath("//*[text()='Log Out']")).click();
+		}
 		
 	// Invalid password
 		public static void invalidPassword(WebDriver driver) {
@@ -75,19 +76,18 @@ public class Index {
 			driver.findElement(By.name("inputPassword")).sendKeys("rahulshettacademy");//inputPassword 
 			driver.findElement(By.className("signInBtn")).click(); //.signInBtn
 			String errorMessage = driver.findElement(By.cssSelector("p.error")).getText();
-			System.out.println(errorMessage); //errorMessage
+				System.out.println(errorMessage); //errorMessage
 		}
 			  
 	// Forgot password
 		public static void forgotPassword(WebDriver driver) {
 			driver.findElement(By.linkText("Forgot your password?")).click();
-			driver.findElement(By.xpath("//input[@placeholder=\"Name\"]")).
-			sendKeys("Lahun Akbal");
+			driver.findElement(By.xpath("//input[@placeholder=\"Name\"]")).sendKeys("Lahun Akbal");
 			driver.findElement(By.xpath("//input[@placeholder=\"Email\"]")).sendKeys("lahunakbal@mail.io");
 			driver.findElement(By.xpath("//input[@placeholder=\"Phone Number\"]")).sendKeys("(11)94785-2674");
 			driver.findElement(By.cssSelector("button.reset-pwd-btn")).click(); String
 			infoMsg = driver.findElement(By.className("infoMsg")).getText(); //.infoMsg
-			System.out.println(infoMsg);
+				System.out.println(infoMsg);
 		}
 
 }
